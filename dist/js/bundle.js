c24b000f89cd087c36061972efd17a71637c8712
iamrajabli <iamrajabli@outlook.com>
2022-05-31 20:20:26 +0400

    Folder organize

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,18 +1,19 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./js/script.js ***!
-  \**********************/
-const navbar__dropdown = document.querySelector('.elements .navbar__dropdown');
-const navbar__left = document.querySelectorAll('.navbar__left>ul>li');
+/******/
+(() => { // webpackBootstrap
+    var __webpack_exports__ = {};
+    /*!**********************!*\
+      !*** ./js/script.js ***!
+      \**********************/
+    const navbar__dropdown = document.querySelector('.elements .navbar__dropdown');
+    const navbar__left = document.querySelectorAll('.navbar__left>ul>li');
 
-navbar__left.forEach((item, index) => {
-    if (item.classList.contains('elements')) {
-        navbar__dropdown.style.cssText = `
+    navbar__left.forEach((item, index) => {
+            if (item.classList.contains('elements')) {
+                navbar__dropdown.style.cssText = `
             left: calc(-100% * ${index+1});
         `
-    }
-})
-/******/ })()
-;
+            }
+        })
+        /******/
+})();
 //# sourceMappingURL=bundle.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
-const navbar__dropdown = document.querySelector('.elements .navbar__dropdown');
-const navbar__left = document.querySelectorAll('.navbar__left>ul>li');
+window.addEventListener('DOMContentLoaded', () => {
+    const navbar__dropdown = document.querySelector('.elements .navbar__dropdown');
+    const navbar__left = document.querySelectorAll('.navbar__left>ul>li');
 
-navbar__left.forEach((item, index) => {
-    if (item.classList.contains('elements')) {
-        navbar__dropdown.style.cssText = `
+    navbar__left.forEach((item, index) => {
+        if (item.classList.contains('elements')) {
+            navbar__dropdown.style.cssText = `
             left: calc(-100% * ${index+1});
         `
-    }
+        }
+    })
 })
