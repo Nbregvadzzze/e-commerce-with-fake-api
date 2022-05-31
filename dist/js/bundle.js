c166f555f67d481b7c03bc16ea2710f8cf0b3ed7
iamrajabli <iamrajabli@outlook.com>
2022-05-31 20:23:55 +0400

    File organize

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,19 +1,20 @@
-/******/
-(() => { // webpackBootstrap
-    var __webpack_exports__ = {};
-    /*!**********************!*\
-      !*** ./js/script.js ***!
-      \**********************/
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+window.addEventListener('DOMContentLoaded', () => {
     const navbar__dropdown = document.querySelector('.elements .navbar__dropdown');
     const navbar__left = document.querySelectorAll('.navbar__left>ul>li');
 
     navbar__left.forEach((item, index) => {
-            if (item.classList.contains('elements')) {
-                navbar__dropdown.style.cssText = `
+        if (item.classList.contains('elements')) {
+            navbar__dropdown.style.cssText = `
             left: calc(-100% * ${index+1});
         `
-            }
-        })
-        /******/
-})();
+        }
+    })
+})
+/******/ })()
+;
 //# sourceMappingURL=bundle.js.map
